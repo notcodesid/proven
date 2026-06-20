2b0c3c443cee924ecff7943f9d59c3ca012d8f76
notcodesid <notcodesid@gmail.com>
2026-06-20 23:00:34 +0530

    Simplify landing page to empty slate

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
-import Appbar from "@/components/appbar";
-import FAQs from "@/components/FAQs";
-import Features from "@/components/featurs";
-import Footer from "@/components/Footer";
-import Hero from "@/components/hero";
-import VideoSection from "@/components/VideoSection"; // Import the new component
-
 export default function Home() {
   return (
-    <div>
-      {/* Video element removed from here */}
-      <Appbar />
-      <Hero />
-      <VideoSection /> {/* Add the new VideoSection component */}
-      <div id="features"><Features /></div>
-      <div id="faqs"><FAQs /></div>
-      <div id="contact"><Footer /></div>
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      {/* Empty Slate */}
     </div>
   );
 }
+
